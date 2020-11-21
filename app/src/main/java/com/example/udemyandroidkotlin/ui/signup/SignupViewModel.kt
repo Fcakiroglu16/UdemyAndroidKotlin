f841090f5285cb69caa1cad6b516c73018beef84
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-11-21 04:55:11 +0300

    LoginActivity 4. kısım

--- app/src/main/java/com/example/udemyandroidkotlin/ui/signup/SignupViewModel.kt
+++ app/src/main/java/com/example/udemyandroidkotlin/ui/signup/SignupViewModel.kt
@@ -1,7 +1,36 @@
 package com.example.udemyandroidkotlin.ui.signup
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.udemyandroidkotlin.ApiServices.LoginService
+import com.example.udemyandroidkotlin.models.ApiError
+import com.example.udemyandroidkotlin.models.UserSignUp
+import com.example.udemyandroidkotlin.utility.IViewModelState
+import com.example.udemyandroidkotlin.utility.LoadingState
+import kotlinx.coroutines.launch
 
-class SignupViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SignupViewModel : ViewModel(), IViewModelState {
+    override var loadingSate: MutableLiveData<LoadingState> = MutableLiveData<LoadingState>()
+    override var errorState: MutableLiveData<ApiError> = MutableLiveData<ApiError>()
+
+
+    fun signUp(userSignUp: UserSignUp): LiveData<Boolean> {
+        var status = MutableLiveData<Boolean>()
+        loadingSate.value = LoadingState.Loading
+
+        viewModelScope.launch {
+
+            var response = LoginService.signUp(userSignUp)
+
+            status.value = response.isSuccessful
+            loadingSate.value = LoadingState.Loaded
+
+            if (!response.isSuccessful) errorState.value = response.fail
+
+        }
+        return status
+
+    }
 }
