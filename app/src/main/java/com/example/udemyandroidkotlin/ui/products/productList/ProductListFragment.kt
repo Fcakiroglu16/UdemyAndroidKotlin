9eee0e7120b1a4039c12966830a0223afca2311f
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-02 21:09:14 +0300

    Navigation Component-2 /Product Add button

--- app/src/main/java/com/example/udemyandroidkotlin/ui/products/productList/ProductListFragment.kt
+++ app/src/main/java/com/example/udemyandroidkotlin/ui/products/productList/ProductListFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.udemyandroidkotlin.R
+import kotlinx.android.synthetic.main.product_list_fragment.view.*
 
 class ProductListFragment : Fragment() {
 
@@ -20,7 +22,18 @@ class ProductListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.product_list_fragment, container, false)
+        var root = inflater.inflate(R.layout.product_list_fragment, container, false)
+
+
+        root.btn_product_add.setOnClickListener {
+
+            it.findNavController().navigate(R.id.productAddFragmentNav)
+
+        }
+
+        return root
+
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/udemyandroidkotlin/ui/user/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.udemyandroidkotlin.ui.user.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
