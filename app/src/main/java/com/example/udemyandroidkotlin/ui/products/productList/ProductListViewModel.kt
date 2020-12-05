09b3aac72e33670c6840f2b61872d01423a6ae34
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-05 16:51:41 +0300

    ProductListFragment viewmodel / Product_card_view /Loading_item

--- app/src/main/java/com/example/udemyandroidkotlin/ui/products/productList/ProductListViewModel.kt
+++ app/src/main/java/com/example/udemyandroidkotlin/ui/products/productList/ProductListViewModel.kt
@@ -1,7 +1,47 @@
 package com.example.udemyandroidkotlin.ui.products.productList
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.udemyandroidkotlin.ApiServices.ProductService
+import com.example.udemyandroidkotlin.models.ApiError
+import com.example.udemyandroidkotlin.models.Product
+import com.example.udemyandroidkotlin.utility.IViewModelState
+import com.example.udemyandroidkotlin.utility.LoadingState
+import kotlinx.coroutines.launch
 
-class ProductListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ProductListViewModel : ViewModel(), IViewModelState {
+    override var loadingSate: MutableLiveData<LoadingState> = MutableLiveData<LoadingState>()
+    override var errorState: MutableLiveData<ApiError> = MutableLiveData<ApiError>()
+    var products = MutableLiveData<ArrayList<Product>>()
+
+
+    fun getProducts(page: Int) {
+
+        if (page == 0) {
+            loadingSate.value = LoadingState.Loading
+        }
+
+        viewModelScope.launch {
+
+            var response = ProductService.productList(page)
+
+
+            if (response.isSuccessful) {
+                products.value = response.success!!
+            } else {
+                errorState.value = response.fail
+            }
+
+            if (page == 0) {
+                loadingSate.value = LoadingState.Loaded
+            }
+
+
+        }
+
+
+    }
+
+
 }
