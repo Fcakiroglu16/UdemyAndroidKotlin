fde97aa7a14ee3c929d39838a150e8b4caa732a0
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-09 01:55:27 +0300

    ProductDetails-4

--- app/src/main/java/com/example/udemyandroidkotlin/ui/products/productDetail/ProductDetailFragment.kt
+++ app/src/main/java/com/example/udemyandroidkotlin/ui/products/productDetail/ProductDetailFragment.kt
@@ -9,7 +9,12 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.navArgs
 import com.example.udemyandroidkotlin.R
+import com.example.udemyandroidkotlin.consts.ApiConsts
+import com.example.udemyandroidkotlin.models.Product
+import com.example.udemyandroidkotlin.ui.user.UserActivity
 import com.example.udemyandroidkotlin.utility.GlobalApp
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.product_detail_fragment.view.*
 
 class ProductDetailFragment : Fragment() {
     val arg: ProductDetailFragmentArgs by navArgs()
@@ -26,8 +31,31 @@ class ProductDetailFragment : Fragment() {
     ): View? {
         viewModel = ViewModelProvider(this).get(ProductDetailViewModel::class.java)
         var root = inflater.inflate(R.layout.product_detail_fragment, container, false)
-        Toast.makeText(GlobalApp.getAppContext(), arg.productId.toString(), Toast.LENGTH_LONG)
-            .show()
+
+
+        UserActivity.setLoadingStatus(viewModel, viewLifecycleOwner)
+        UserActivity.setErrorStatus(viewModel, viewLifecycleOwner)
+        var p: (Product?) -> Unit = {
+
+            if (it != null) {
+
+                root.txt_product_name.text = it.Name
+                root.txt_product_color.text = it.Color
+                root.txt_product_price.text = it.Price.toString()
+                root.txt_product_stock.text = it.Stock.toString()
+                root.txt_product_category_name.text = it.Category?.Name
+                var fullPhotoUrl = "${ApiConsts.photoBaseUrl}/${it.PhotoPath}"
+
+                Picasso.get().load(fullPhotoUrl).placeholder(R.drawable.no_image_available)
+                    .error(R.drawable.no_image_available).into(root.img_product_picture)
+
+            }
+
+        }
+
+
+        viewModel.getProduct(arg.productId).observe(viewLifecycleOwner, p)
+
 
         return root
     }
